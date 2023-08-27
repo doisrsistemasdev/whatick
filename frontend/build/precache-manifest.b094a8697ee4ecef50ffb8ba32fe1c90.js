8a8753ebbeccc546ca4087ae7d54f59ad0b0bf03
doisr <rodrig0d@yahoo.com.br>
2023-08-26 23:48:23 -0300

    sub projeto

---
+++ frontend/build/precache-manifest.b094a8697ee4ecef50ffb8ba32fe1c90.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e6e54639bab404b04dff99068af0e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f84570fc24c2e9456a",
+    "url": "/static/css/2.fa426b8c.chunk.css"
+  },
+  {
+    "revision": "a9f84570fc24c2e9456a",
+    "url": "/static/js/2.f12abecd.chunk.js"
+  },
+  {
+    "revision": "e32ccaf4a0109157023fabb2ad785ad7",
+    "url": "/static/js/2.f12abecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bba904af03cda47234",
+    "url": "/static/js/3.4356e338.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.4356e338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b9127f7fa3f9185a4f",
+    "url": "/static/js/main.11f43945.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.11f43945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b08a208683c5b49819",
+    "url": "/static/js/runtime-main.2361b08a.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "648831e851fff5177ff113e6a214a092",
+    "url": "/static/media/zapsimples.648831e8.png"
+  }
+]);
